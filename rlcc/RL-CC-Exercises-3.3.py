2b659e057040253f44b00e9d34dac9594336d8cb
Mario Jason Braganza <jason@janusworx.com>
2020-08-02 08:48:32 +0530

    removed extraneous comprehension
    Signed-off-by: Mario Jason Braganza <jason@janusworx.com>

--- rlcc/RL-CC-Exercises-3.3.py
+++ rlcc/RL-CC-Exercises-3.3.py
@@ -14,17 +14,27 @@ Problems
 #             shells.append(split_line[-1])
 #     print(set(shells))
 
+# with open("samplefiles/linux-etc-passwd.txt") as shell_source:
+#     print(
+#         set(
+#             [
+#                 last_item[-1]
+#                 for last_item in [
+#                     each_line.strip().split(":")
+#                     for each_line in shell_source
+#                     if each_line.strip() and not each_line.startswith("#")
+#                 ]
+#             ]
+#         )
+#     )
+
 with open("samplefiles/linux-etc-passwd.txt") as shell_source:
     print(
         set(
             [
-                last_item[-1]
-                for last_item in [
-                    each_line.strip().split(":")
-                    for each_line in shell_source
-                    if each_line.strip() and not each_line.startswith("#")
-                ]
+                each_line.strip().split(":")[-1]
+                for each_line in shell_source
+                if each_line.strip() and not each_line.startswith("#")
             ]
         )
     )
-
